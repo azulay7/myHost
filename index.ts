6419161f0e2d13d01a0a857774b7e2bcf70ccdc2
Amit Azulay <amita@klips.com>
2024-04-07 16:27:31 +0300

    start

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, Express server with TypeScript!');
+});
+
+app.get('/appleLogin', (req: Request, res: Response) => {
+    res.send('appleLogin');
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
